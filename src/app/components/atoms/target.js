33d7802d1fca790aa85936069c3b3e3fa53ef052
Adam Johnston <adam.johnston3679@gmail.com>
2016-10-18 20:41:16 +0100

    update target to accept drops

--- src/app/components/atoms/target.js
+++ src/app/components/atoms/target.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { createClass } from 'react';
+import { DropTarget } from 'react-dnd';
+import config from 'app/config';
 
-export default () => (
-  <div
-    className="placeholder">
-  </div>
-);
+const types = config.types;
+
+const dropSource = {
+  drop(props, monitor, component) {
+    console.log(props, monitor, component);
+  }
+};
+
+const collect = (connect, monitor) => {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver()
+  };
+};
+
+const Target = createClass({
+  render() {
+    const {
+      connectDropTarget,
+      isOver
+    } = this.props;
+
+    return connectDropTarget(
+      <div
+        className="placeholder"
+        style={isOver ? {background: 'black'} : null}>
+      </div>
+    );
+  }
+});
+
+Target.propTypes = {
+  connectDropTarget: React.PropTypes.func.isRequired,
+  isOver: React.PropTypes.bool.isRequired
+};
+
+export default DropTarget(types.CARD, dropSource, collect)(Target);
