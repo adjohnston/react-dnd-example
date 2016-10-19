097a5b64b69f71654aa1371b07327bd7f4fe99f4
Adam Johnston <adam@getkahoot.com>
2016-10-19 11:49:52 +0100

    send a default state to the app

--- src/app/index.js
+++ src/app/index.js
@@ -2,4 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import JumbleBoard from 'components/app/jumble-board';
 
-ReactDOM.render(<JumbleBoard  />, document.querySelector('#app'));
+const cards = [
+  'circle',
+  null,
+  null,
+  null,
+  'square',
+  null,
+  'triangle',
+  'diamond',
+];
+
+ReactDOM.render(
+  <JumbleBoard
+    cards={cards} />,
+  document.querySelector('#app')
+);
